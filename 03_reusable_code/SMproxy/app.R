51369fd5a54ef806808867500aebd4b679762167
RKvdK <r.k.vanderkaap@hotmail.com>
2025-12-22 16:42:06 +0100

    Thesis scripts integrated in the shiny app. For now, there is an action button and a slider to modify the number of runs. However, all other inputs still have to be programmed. In addition, side and main pannels need to be defined. Also, the resulting app needs to be monkey tested. PS. tidyverse is maybe not required after all.

--- 03_reusable_code/SMproxy/app.R
+++ 03_reusable_code/SMproxy/app.R
@@ -1,50 +1,153 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Load required packages
 
 library(shiny)
+library(here)
+library(tidyverse)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+# Load external scripts
+
+source("../Thesis scripts/02_utils.R") # Load utility functions 
+source("../Thesis scripts/05_generate_population.R") # Load SIM function
+source("../Thesis scripts/06_perform_mc.R") # Load Monte Carlo function
+
+# Define the input parameters
+
+inpar <- function(input){
+  
+  # These parameters are not directly required for the simulation code
+  # Nevertheless they are included because the user of the app is able to modify these parameters
+  
+  n = as.integer(input$n)
+  percExt = as.numeric(input$percExternal)
+  
+  list(
+    
+    # These parameters are directly required for the simulation code
+    
+    nA = n,
+    nB = n,
+    nE = round(n * percExt),
+    
+    PercOverlap = as.numeric(input$percOverlap),
+    tranmat_diag = as.numeric(input$p_diag),
+    tranmat_sym = (input$w == "Symmetrical"),
+    cia =  as.numeric(input$CIA),
+    
+    N = 1e6,
+    zpx = 0.4,
+    Xsource = input$Xsource
+  )
+}
+
 ui <- fluidPage(
+  
+  # Application title
+  
+  titlePanel("Exploration of Statistical Matching with a Proxy Variable"),
+  
+  # Create action button
+  
+  actionButton("run", "Run the simulation"),
+  
+  # Create slider input for the user to modify the number of Monte Carlo runs
+  
+  sliderInput("MCnum", "Number of Monte Carlo runs",
+              min = 1,
+              max = 10,
+              value = 3,
+              step = 1
+              ),
+  
+  tableOutput("tab"), # Output results table
+  plotOutput("rmse_plot") # Output RMSE plot
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  
+  # Retrieve the results
+  
+  res <- eventReactive(input$run, {   # eventReactive to run the simulation only when user presses the action button
+    
+    withProgress(message = "Running the simulation...", value = 0, { # Initalize progression bar
+      
+      incProgress(0.1, detail = "Inserting the parameters")
+      param <- inpar(input)
+      
+      incProgress(0.4, detail = "Generating the population")
+      simout <- do.call(SIM, param)
+      
+      incProgress(0.4, detail = "Performing the Monte Carlo simulation")
+      mcout <- MC(
+        SIMout = simout,
+        MCnum = as.integer(input$MCnum), # Override the default number of Monte Carlo runs
+        seed = 1L
+      )
+      
+      incProgress(0.1, detail = "Finishing touch")
+      list(
+        sim = simout,
+        mc = mcout
+      )
+      
     })
+    
+  }, ignoreInit = TRUE)   # ignoreInit prevents the function from running at app initialization
+  
+  # Construct results table
+  
+  output$tab <- renderTable({
+    
+    req(res()) # Ensure the results to be available
+    mc <- res()$mc # Retrieve Monte Carlo results
+    
+    data.frame(
+      Estimator = c("DRE", # Doubly robust estimator
+                "IPF", # Iterative proportional fitting estimator
+                "EXT" # External model based estimator
+                ), 
+      RMSE = c(mc$DRE$rmse_mean, # Mean RMSE value per estimator
+               mc$IPF$rmse_mean,
+               mc$EXT$rmse_mean
+              )
+      )
+    }, digits = 4)
+  
+  # Construct RMSE plot
+  
+  output$rmseplot <- renderPlot({
+    
+    req(res()) # Ensure the results to be available
+    mc <- res()$mc # Retrieve Monte Carlo results
+    
+    rmsedata <- data.frame(
+      
+      Iteration = rep(1:input$MCnum, 3), # Iteration numbers for all three estimators
+      
+      Estimator = factor(rep(c("DRE", "IPF", "EXT"), 
+                             levels = c("DRE", "IPF", "EXT"))), 
+      RMSEmean = c(
+        mc$DRE$rmse,
+        mc$IPF$rmse,
+        mc$EXT$rmse
+      ),
+      
+      # Generate the plot
+      
+      ggplot(rmsedata, aes(x = Estimator, y = RMSE)) +
+        geom_col(fill = "pink") +
+        labs(
+          title = "Average RMSE value per estimator",
+          x = "Estimator",
+          y = "RMSE"
+        ) +
+        theme_minimal()
+      
+    )
+    
+  })
+  
 }
 
 # Run the application 
